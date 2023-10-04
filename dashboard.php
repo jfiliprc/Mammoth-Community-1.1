b439d4b39bc09a8c808d2ab9f78bc6554a44c8bf
J. Felipe Rocha <109008096+jfiliprc@users.noreply.github.com>
2023-10-04 11:31:14 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,80 +6,28 @@
     <title>Mamooth Community | Dashboard</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        .navbar {
-            background-color: #343a40;
-        }
-        .navbar-dark .navbar-brand {
-            font-size: 24px;
-        }
-        .container {
-            margin-top: 20px;
-        }
-        .post-box {
-            background-color: #fff;
-            border: 1px solid #dee2e6;
-            padding: 15px;
-            margin-bottom: 20px;
-        }
-        .timeline {
-            background-color: #fff;
-            border: 1px solid #dee2e6;
-            padding: 15px;
-        }
-        .logout-btn, .logout-txt {
-        color: white; 
-        }
-        .logout-btn:hover, .logout-txt:hover {
-        color: red; 
-        transition: color 0.3s ease; 
-        }
-        
-    </style>
 </head>
+
+<style>
+
+.btn-txt {
+    color: #FFFFFF !important;
+    text-decoration: none !important;
+}
+
+</style>
+
 
 <?php
 session_start();
 if (!$_SESSION['user_id']) {
-?>
-    <div class="alert alert-danger" role="alert">
-        Insira um login válido, ô espertão.
-    </div>
-<?php
     header("Location: login.php");
     exit;
 }
 ?>
 
 <body>
-<div id="react-navbar"></div>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="images/mamute-logo.png" width="100" alt="Logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="logout.php" class="nav-link logout-btn" style="color: white;">
-                        <i class="fa fa-sign-out-alt logout-txt"></i> Logout
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-
-
+<?php require_once 'navbar.php'; ?>
     <div class="container">
         <div class="row">
             <div class="col-md-8">
@@ -109,15 +57,26 @@ if (!$_SESSION['user_id']) {
                     $stmt = $pdo->query("SELECT topics.*, users.first_name FROM topics INNER JOIN users ON topics.user_id = users.id ORDER BY topics.create_date DESC");
 
                     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        ?>
-                        <div class="media" style="padding: 10px;">
-                            <img src="images/default-avatar.jpg" style="width:20%;" alt="User Image">
-                            <div class="media-body" style="padding-left: 10px;" >
-                                <h5 class="mt-0"><?= $row['first_name'] ?></h5>
-                                <?php echo htmlspecialchars($row['body']); ?> 
+
+                        if (isset($row['id'])) {
+                            $topic_id = $row['id'];
+                            ?>
+
+                            <div class="media" style="padding: 10px;">
+                                <img src="images/default-avatar.jpg" style="width:20%;" alt="User Image">
+                                <div class="media-body" style="padding-left: 10px;" >
+                                    <h5 class="mt-0"><?= $row['first_name'] ?></h5>
+                                    <?php echo htmlspecialchars($row['body']); ?> 
+                                </div>
+                                <button type="button" class="btn btn-danger">
+                                    <a href="expanded_post.php?id=<?= $topic_id ?>" class="btn-txt">
+                                        <i class="fas fa-comment"></i>
+                                    </a>
+                                </button>
                             </div>
-                        </div>
+
                         <?php
+                        }
                     }
                     ?>
                 </div>
